829df3e13a68e179efd55467eb19481f45866ff7
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-01 22:27:58 +0100

    Updated dashboard navbar

--- components/rolesPages/RolesNavbar.jsx
+++ components/rolesPages/RolesNavbar.jsx
@@ -2,7 +2,7 @@
 
 const RolesNavbar = ({ role }) => {
   return (
-    <nav className="bg-orange-500 text-white py-4">
+    <nav className="bg-gray-100 text-gray-800 py-4 px-2">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">
           {role.charAt(0).toUpperCase() + role.slice(1)} Dashboard
