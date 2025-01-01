f3804d6438783f35ebef3c8d34e25b026daee0bf
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-01 22:14:46 +0100

    updated login auth services

--- services/auth.service.js
+++ services/auth.service.js
@@ -11,6 +11,21 @@ const authService = {
     const response = await API.post("auth/admin/signup", formData);
     return response.data;
   },
+  // Admin Login Function
+  adminLogin: async (formData) => {
+    const response = await API.post("auth/admin/login", formData);
+    return response.data;
+  },
+  // Teacher Log-In Function
+  teacherLogin: async (formData) => {
+    const response = await API.post("auth/teacher/login", formData);
+    return response.data;
+  },
+    // Student Log-In Function
+  studentLogin: async (formData) => {
+    const response = await API.post("auth/student/login", formData);
+    return response.data;
+  },
 };
 
 export default authService;
