0bc90a2ec8def3065608f432e5d857f8c43d4894
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-02 09:51:20 +0100

    Added api services for admin to get sessions,, terms and toggle scoring

--- services/admin.service.js
+++ services/admin.service.js
@@ -12,6 +12,36 @@ const adminService = {
     });
     return response.data.data;
   },
+  getSessionsWithTerms: async () => {
+    const token = localStorage.getItem("authToken");
+    const response = await API.get("term/sessions", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data.data;
+  },
+  toggleScoring: async (termId, isScoringEnabled) => {
+    const token = localStorage.getItem("authToken");
+    const response = await API.patch(
+      "term/toggle-scoring",
+      { termId, isScoringEnabled },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+    return response.data.data;
+  },
+  addSession: async (data) => {
+    const token = localStorage.getItem("authToken");
+    const response = await API.post("term/session", data, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data.data;
+  },
+  addTerm: async (data) => {
+    const token = localStorage.getItem("authToken");
+    const response = await API.post("term/term", data, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data.data;
+  },
 };
 
 export default adminService;
