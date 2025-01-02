99584d39facd7992d27065261880fe7f100413e3
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-02 03:06:33 +0100

    Edited dashboard content styles

--- components/rolesPages/DashboardContent.jsx
+++ components/rolesPages/DashboardContent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const DashboardContent = ({ role }) => {
   return (
-    <div className="p-6">
+    <div className="p-6 bg-gray-100 h-screen overflow-y-scroll">
       <h2 className="text-3xl font-bold">Welcome, {role.charAt(0).toUpperCase() + role.slice(1)}!</h2>
       <p className="mt-4 text-gray-600">This is your dashboard. Customize it to fit your needs.</p>
     </div>
