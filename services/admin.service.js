4e29b42a51da2d03de6b0ec87b9a7af9a04541b7
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-08 22:23:36 +0100

    added more api requests

--- services/admin.service.js
+++ services/admin.service.js
@@ -42,6 +42,44 @@ const adminService = {
     });
     return response.data.data;
   },
+  getClasses: async () => {
+    const token = localStorage.getItem("authToken");
+    const response = await API.get("class/list-classes", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data.data;
+  },
+
+  getTeachers: async () => {
+    const token = localStorage.getItem("authToken");
+    const response = await API.get("admin/list-teachers", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data.data;
+  },
+
+  addClass: async (data) => {
+    const token = localStorage.getItem("authToken");
+    const response = await API.post("class/create", data, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data.data;
+  },
+
+  updateClass: async (classId, data) => {
+    const token = localStorage.getItem("authToken");
+    const response = await API.patch(`class/${classId}`, data, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data.data;
+  },
+
+  deleteClass: async (classId) => {
+    const token = localStorage.getItem("authToken");
+    await API.delete(`class/${classId}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  },
 };
 
 export default adminService;
