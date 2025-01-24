30942854493045a1b67560488b1179e243626f8d
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-24 22:03:15 +0100

    Connected frontend to a hosted backend

--- services/auth.service.js
+++ services/auth.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set base URL
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/v1/",
+  baseURL: "https://gradeflow-server.onrender.com/api/v1/",
 });
 
 const authService = {
